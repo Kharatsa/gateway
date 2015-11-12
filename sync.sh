952058e35dc9a0fc47cde233c709a0e834825ad2
Sean Herman <seanherman@gmail.com>
2015-11-11 22:14:44 -0500

    move web app to default/root domain

--- sync.sh
+++ sync.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
+set -x
 
-AWS_ELASTIC_IP="52.4.26.168"
-LOCAL_KEY="odk-dev.pem"
+ADMIN_USERNAME="ubuntu"
+LOCAL_SSH_KEY="odk-dev.pem"
+NGINX_CONFIG_DIR="/home/$ADMIN_USERNAME/nginxconfig"
+NGINX_CONFIG_REPO="https://github.com/Kharatsa/gateway.git"
+# CONFIG_BACKUP_DIR="/home/ubuntu/nginx-config-backups/$(date +%s)"
 
-rsync --delete -auve "ssh -i $LOCAL_KEY" \
-  nginx/* \
-  ubuntu@$AWS_ELASTIC_IP:/home/ubuntu/nginx-config
 
-ssh -i $LOCAL_KEY ubuntu@$AWS_ELASTIC_IP \
-'bash -s' <<'ENDSSH'
-  sudo su
-  REMOTE_BACKUP_DIR="/home/ubuntu/nginx-config-backups/$(date +%s)"
-  mkdir -p "$REMOTE_BACKUP_DIR"
-  cp -R /etc/nginx/* "$REMOTE_BACKUP_DIR"
-  chown -R ubuntu:ubuntu "$REMOTE_BACKUP_DIR"
-  cp -R /home/ubuntu/nginx-config/* /etc/nginx/
-  nginx -s reload
-ENDSSH
+echo "----------------------------------------------"
+echo "------------ Pull repo updates ---------------"
+echo "----------------------------------------------"
+
+if [ -d "$NGINX_CONFIG_DIR" ]; then
+  mkdir -p $NGINX_CONFIG_DIR
+  git clone $NGINX_CONFIG_REPO $NGINX_CONFIG_DIR
+else
+  cd $NGINX_CONFIG_DIR
+  git pull
+fi
+
+# sudo mkdir -p "$CONFIG_BACKUP_DIR"
+# sudo cp -R /etc/nginx/* "$CONFIG_BACKUP_DIR"
+# sudo chown -R ubuntu:ubuntu "$CONFIG_BACKUP_DIR"
+sudo cp -R $NGINX_CONFIG_DIR/* /etc/nginx/
+sudo nginx -s reload
