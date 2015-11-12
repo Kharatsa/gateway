acc92d0295e157cbd633a2d396d238401840345c
Sean Herman <seanherman@gmail.com>
2015-11-11 23:44:23 -0500

    minor sync changes

--- sync.sh
+++ sync.sh
@@ -3,7 +3,9 @@ set -x
 
 ADMIN_USERNAME="ubuntu"
 LOCAL_SSH_KEY="odk-dev.pem"
-NGINX_CONFIG_DIR="/home/$ADMIN_USERNAME/nginxconfig"
+
+NGINX_CONFIG_DIR="nginxconfig"
+NGINX_CONFIG_PATH="/home/$ADMIN_USERNAME/nginxconfig"
 NGINX_CONFIG_REPO="https://github.com/Kharatsa/gateway.git"
 # CONFIG_BACKUP_DIR="/home/ubuntu/nginx-config-backups/$(date +%s)"
 
@@ -12,16 +14,19 @@ echo "----------------------------------------------"
 echo "------------ Pull repo updates ---------------"
 echo "----------------------------------------------"
 
+cd
 if [ -d "$NGINX_CONFIG_DIR" ]; then
-  mkdir -p $NGINX_CONFIG_DIR
-  git clone $NGINX_CONFIG_REPO $NGINX_CONFIG_DIR
-else
-  cd $NGINX_CONFIG_DIR
+  echo "Pulling"
+  cd $NGINX_CONFIG_PATH
   git pull
+else
+  echo "Making"
+  mkdir -p $NGINX_CONFIG_PATH
+  git clone $NGINX_CONFIG_REPO $NGINX_CONFIG_PATH
 fi
 
 # sudo mkdir -p "$CONFIG_BACKUP_DIR"
 # sudo cp -R /etc/nginx/* "$CONFIG_BACKUP_DIR"
 # sudo chown -R ubuntu:ubuntu "$CONFIG_BACKUP_DIR"
-sudo cp -R $NGINX_CONFIG_DIR/* /etc/nginx/
+sudo cp -R $NGINX_CONFIG_PATH/nginx/* /etc/nginx/
 sudo nginx -s reload
