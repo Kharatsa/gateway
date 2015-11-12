29805f395956aab503da857c7941ac9344dead16
Sean Herman <seanherman@gmail.com>
2015-11-11 23:47:35 -0500

    fix sync errors

--- sync.sh
+++ sync.sh
@@ -1,30 +1,22 @@
 #!/usr/bin/env bash
 
+ADMIN_USERNAME="ubuntu"
 NGINX_CONFIG_DIR="nginxconfig"
 NGINX_CONFIG_PATH="/home/$ADMIN_USERNAME/nginxconfig"
 NGINX_CONFIG_REPO="https://github.com/Kharatsa/gateway.git"
-# CONFIG_BACKUP_DIR="/home/ubuntu/nginx-config-backups/$(date +%s)"
-rsync --delete -auve "ssh -i $LOCAL_KEY" \
-  nginx/* \
-  ubuntu@$AWS_ELASTIC_IP:/home/ubuntu/nginx-config
 
 echo "----------------------------------------------"
 echo "------------ Pull repo updates ---------------"
 echo "----------------------------------------------"
 
 cd
-if [ -d "$NGINX_CONFIG_DIR" ]; then
-  echo "Pulling"
+if [ -d "$NGINX_CONFIG_PATH" ]; then
   cd $NGINX_CONFIG_PATH
   git pull
 else
-  echo "Making"
   mkdir -p $NGINX_CONFIG_PATH
   git clone $NGINX_CONFIG_REPO $NGINX_CONFIG_PATH
 fi
 
-# sudo mkdir -p "$CONFIG_BACKUP_DIR"
-# sudo cp -R /etc/nginx/* "$CONFIG_BACKUP_DIR"
-# sudo chown -R ubuntu:ubuntu "$CONFIG_BACKUP_DIR"
 sudo cp -R $NGINX_CONFIG_PATH/nginx/* /etc/nginx/
 sudo nginx -s reload
